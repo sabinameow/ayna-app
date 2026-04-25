abf41e8dc818299899883c736b121f672c6d10cf
sabina <muratbajsabina26@gmail.com>
2026-04-25 13:42:33 +0500

    fix mail config

---
+++ backend/app/auth/mail.py
@@ -0,0 +1,25 @@
+from fastapi_mail import FastMail, ConnectionConfig, MessageSchema, MessageType
+from backend.app.config import Settings
+
+settings = Settings()
+
+mail_config = ConnectionConfig(
+    MAIL_USERNAME=settings.MAIL_USERNAME,
+    MAIL_PASSWORD=settings.MAIL_PASSWORD,
+    MAIL_FROM=settings.MAIL_FROM,
+    MAIL_PORT=587,
+    MAIL_SERVER=settings.MAIL_SERVER,
+    MAIL_FROM_NAME=settings.MAIL_FROM_NAME,
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,
+)
+
+mail = FastMail(config=mail_config)
+
+def create_message(recipients: list[str], subject: str, body: str):
+    message = MessageSchema(
+        recipients=recipients, subject=subject, body=body, subtype=MessageType.html
+    )
+    return message
